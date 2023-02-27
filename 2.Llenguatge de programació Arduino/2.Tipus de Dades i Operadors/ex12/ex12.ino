280418b1c67f1f06114ab4e6c0760f33e7698a0d
BILALNAMIR <125276875+BILALNAMIR@users.noreply.github.com>
2023-02-27 14:10:42 +0100

    ex

--- 2.Llenguatge de programació Arduino/2.Tipus de Dades i Operadors/ex12/ex12.ino
+++ 2.Llenguatge de programació Arduino/2.Tipus de Dades i Operadors/ex12/ex12.ino
@@ -8,23 +8,30 @@
 //********** Includes *************************************************************
 
 //********** Variables ************************************************************
-int test = 32767;
+int a = 3;
+int b = 2;
+float d;
 
 //********** Setup ****************************************************************
-void setup()              // run once, when the sketch starts
+void setup()               // run once, when the sketch starts
 {
-  Serial.begin(9600);     //set up Serial library at 9600 bps
-  Serial.print("Test value is: ");
-  Serial.println(test);
+  Serial.begin(9600);      // set up Serial library at 9600 bps
 
-  test = test + 1;
+  Serial.println("Here is division: ");
+
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
+
+  d =(float) a / b;
  
-  Serial.print("Now it is ");
-  Serial.println(test);
+  Serial.print("a / b = ");
+  Serial.println(d);
 }
 
 //********** Loop *****************************************************************
-void loop()   // we need this to be here even though its empty
+void loop()    // we need this to be here even though its empty
 {
 }
 
